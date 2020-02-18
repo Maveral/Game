2b186ec0500d180c9d5f9ddddb83c43dc0e6cce0
Maveral <maverral@wp.pl>
2020-02-18 12:47:27 +0100

    First push

--- app/hyperstack/components/app.rb
+++ app/hyperstack/components/app.rb
@@ -15,10 +15,49 @@ class App < HyperComponent
   # Route('/user/:id/name', mounts: UserName) : path segments beginning with a colon will be captured in the match param
   # see the hyper-router gem documentation for more details
 
+  #@left = Array.new(10) { rand(10..90) }
+  #@delay = Array.new(10) { rand(1..9) }
+
+  before_mount do
+
+    Move.on_dispatch do|params|
+      mutate @id = params.id
+      mutate @score += 1
+      jQ["##{@id}"].addClass( "hide" );
+    end
+
+    Start.on_dispatch do |params|
+      mutate @st = params.st
+    end
+
+    @left = [10,20,33,45,56,67,71,77,80,90]
+    @delay = [1,3.7,2.1,4,5,4.6,2.8,7.1,3.9,1.8]
+    @st = 0
+    @score = 0
+
+  end
+
   render(DIV) do
-    H1 { "Hello world from Hyperstack!" }
-    BUTTON {"Click me"}.on(:click) do
-      alert("All working!")
+
+    BUTTON(class: "start") do
+      "Start"
+    end.on(:click) do
+      Start.run(st: 1)
+    end
+
+    DIV(class: "score") do
+      "Your score: #{@score}"
+    end
+
+    if @st == 1
+      10.times do |x|
+        DIV(class: "move1", id: x, style: {"left" => "#{@left[x]}vw", "animation-delay" => "#{@delay[x]}s"}) do
+          ""
+        end.on(:click) do
+          Move.run(id: x)
+        end
+      end
     end
   end
+
 end
---
+++ app/hyperstack/operations/start.rb
@@ -0,0 +1,19 @@
+class Start < Hyperstack::ControllerOp
+
+  # ControllerOp is the most basic kind of remote runnable
+
+  # Operation.  It will execute on the server, but can be invoked from the client
+
+  # All security checking is expected to be done by the
+
+  # operation itself.  (ServerOps provide integrated error checking...)
+
+  # In our initial implementation Move does nothing but rebroadcast its
+
+  # input across the application
+
+  param :st
+
+  dispatch_to Hyperstack::Application # channel to dispatch to
+
+end
